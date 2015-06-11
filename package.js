9fcd6dfa2e17310de25364727403e6f2fdd63e8b
javier <javier.berneche@gmail.com>
2015-06-11 13:18:18 -0300

    push version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: 'aida:dtype',
     summary: 'dynamic types',
-    version: "0.1.0",
+    version: "0.1.1",
     git: "https://github.com/Exartu/Exartu-DType.git"
 });
 
